3f1e96fa7fdbb1d7a48bd10112c728d2f2b14a65
Abhishek Aryan <abhishek.rbg@gmail.com>
2023-09-22 13:56:21 +0100

    Stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
-// Uncomment this block to pass the first stage
+use std::io::Write;
 use std::net::TcpListener;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+const SIMPLE_STRING_PREFIX: &str = "+";
+const CLRF: &str = "\r\n";
+const PING_RESPONSE: &str = "PONG";
 
-    // Uncomment this block to pass the first stage
-    
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+fn main() {
+    let host = "127.0.0.1";
+    let port: u16 = 6379;
+
+    let listener = TcpListener::bind((host, port)).unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                println!("established connection");
+                let _ = _stream.write(simple_string_message(PING_RESPONSE).as_bytes());
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn simple_string_message(message: &str) -> String {
+    format!("{}{}{}", SIMPLE_STRING_PREFIX, message, CLRF)
 }
